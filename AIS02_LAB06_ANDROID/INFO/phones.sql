31c6f9634bc3ee7a00a834ffb2cefdde363925c7
Iskander <71010650+abeldinov160599@users.noreply.github.com>
2022-12-01 13:44:23 +0600

    Delete AIS02_LAB06_ANDROID directory

--- AIS02_LAB06_ANDROID/INFO/phones.sql
+++
@@ -1,6 +0,0 @@
-﻿/* 
-/data/data/kz.talipovsn.database/databases/phones
-*/
-DROP TABLE IF EXISTS emergency_service
-CREATE TABLE emergency_service(id INTEGER PRIMARY KEY,name TEXT,name_lc TEXT,phone_number TEXT)
-SELECT  * FROM emergency_service WHERE (name_lc LIKE '%11%' OR phone_number LIKE '%11%') ORDER BY name
